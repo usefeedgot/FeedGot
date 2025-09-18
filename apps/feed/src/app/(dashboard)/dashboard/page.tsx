5565962eed770d509c3f63f29b94d5a9ad8f7935
artistatbl <jdaly2991@gmail.com>
2025-09-18 14:51:48 +0100

    refactor(dashboard): temporarily comment out unused components

--- apps/feed/src/app/(dashboard)/dashboard/page.tsx
+++ apps/feed/src/app/(dashboard)/dashboard/page.tsx
@@ -27,11 +27,11 @@ export default function Page() {
         <div className="flex flex-1 flex-col">
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-              <SectionCards />
+              {/* <SectionCards /> */}
               <div className="px-4 lg:px-6">
-                <ChartAreaInteractive />
+                {/* <ChartAreaInteractive /> */}
               </div>
-              <DataTable data={data} />
+              {/* <DataTable data={data} /> */}
             </div>
           </div>
         </div>
